2baacec9d3cbf97d4a1afeda7efab2da07eeef76
Luke Chambers <lukenatchambers@gmail.com>
2018-06-01 11:03:17 -0600

    Work on CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,19 +12,31 @@ class PostsController < ApplicationController
     if post.save
       redirect_to post_path(post)
     else
-      redirect_to new_post_path
+      redirect_to new_user_post_path(current_user)
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    if post.update(post_params)
+      redirect_to post_path(post)
+    else
+      redirect_to edit_user_post_path(current_user, post)
+    end
   end
 
   def destroy
+    post = Post.find(params[:id])
+    post.destroy!
+    redirect_to root_path
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'welcome#index'
+  devise_for :users
   resources :posts, only: [:show]
   resources :users, only: [] do
-    resources :posts, only: [:new, :create, :update, :edit, :delete]
+    resources :posts, only: [:new, :create, :update, :edit, :destroy]
   end
 end
